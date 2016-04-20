f2fbe65526911f26dc1da933ecb8ed2627a0cec5
qingbo <bluesky2008bo@126.com>
2016-04-20 15:14:22 +0800

    增加双击退出app工具类 DoubleClickExitDetector

---
+++ MyUtils/src/main/java/com/zhao/myutils/utils/DoubleClickExitDetector.java
@@ -0,0 +1,49 @@
+package com.zhao.myutils.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Description: 双击退出app
+ *
+ */
+public class DoubleClickExitDetector {
+    public static final String DEFAULT_HINT_MESSAGE = "再按一次退出程序";
+    public static final long DEFAULT_SPACE_TIME = 2000L;
+    private long mSpaceTime;
+    private long mLastClickTime;
+    private String mHintMessage;
+    private Context mContext;
+
+    public DoubleClickExitDetector(Context context) {
+        this(context, DEFAULT_HINT_MESSAGE, DEFAULT_SPACE_TIME);
+    }
+
+    public DoubleClickExitDetector(Context context, String hintMessage) {
+        this(context, hintMessage, DEFAULT_SPACE_TIME);
+    }
+
+    public DoubleClickExitDetector(Context context, String hintMessage, long space_time) {
+        this.mContext = context;
+        this.mHintMessage = hintMessage;
+        this.mSpaceTime = space_time;
+    }
+
+    public boolean onClick() {
+        long currentTime = System.currentTimeMillis();
+        boolean result = (currentTime - mLastClickTime) < mSpaceTime;
+        mLastClickTime = currentTime;
+        if (!result) {
+            Toast.makeText(mContext, mHintMessage, Toast.LENGTH_SHORT).show();
+        }
+        return result;
+    }
+
+    public void setSpaceTime(long spaceTime) {
+        this.mSpaceTime = spaceTime;
+    }
+
+    public void setHintMessage(String hintMessage) {
+        this.mHintMessage = hintMessage;
+    }
+}
