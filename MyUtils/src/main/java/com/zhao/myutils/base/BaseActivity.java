884eeaa6e1e8c2d1c520e21b766845cac7808b83
qingbo <bluesky2008bo@126.com>
2016-04-25 17:36:46 +0800

    BaseActivity中增加view click事件监听

--- MyUtils/src/main/java/com/zhao/myutils/base/BaseActivity.java
+++ MyUtils/src/main/java/com/zhao/myutils/base/BaseActivity.java
@@ -3,6 +3,7 @@ package com.zhao.myutils.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 /**
  * Description: activity基类，继承FragmentActivity
@@ -10,11 +11,13 @@ import android.support.v4.app.FragmentActivity;
  * @since 2016/4/25
  */
 public abstract class BaseActivity extends FragmentActivity {
+    public Click click;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayoutResId());
+        click = new Click();
         initView(savedInstanceState);
         initListener();
         initData(savedInstanceState);
@@ -22,6 +25,7 @@ public abstract class BaseActivity extends FragmentActivity {
 
     /**
      * 布局资源id
+     *
      * @return resId
      */
     public abstract int setLayoutResId();
@@ -41,4 +45,28 @@ public abstract class BaseActivity extends FragmentActivity {
      */
     protected abstract void initData(Bundle bundle);
 
+    /**
+     * 对view设置监听
+     */
+    public void setOnClickListener(View view) {
+        if (click == null) {
+            click = new Click();
+        }
+        view.setOnClickListener(click);
+    }
+
+    /**
+     * 对view的点击事件监听
+     */
+    protected void treatClickEvent(View view) {
+
+    }
+
+    private class Click implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            treatClickEvent(view);
+        }
+
+    }
 }
