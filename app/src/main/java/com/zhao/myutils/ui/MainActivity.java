e10cef3695b2d30611615f9323e90586336dc0ee
qingbo <bluesky2008bo@126.com>
2016-04-25 11:34:29 +0800

    更新app中src框架

--- app/src/main/java/com/zhao/myutils/ui/MainActivity.java
+++ app/src/main/java/com/zhao/myutils/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zhao.myutils;
+package com.zhao.myutils.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zhao.myutils.R;
 
 public class MainActivity extends AppCompatActivity {
 
