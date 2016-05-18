74aceb6e67d2bef54b4124a06d643ea839dbdbdb
qingbo <bluesky2008bo@126.com>
2016-05-18 18:06:44 +0800

    添加判断服务是否允许的方法；isServiceRunning()

--- MyUtils/src/main/java/com/zhao/myutils/utils/AppManagerUtil.java
+++ MyUtils/src/main/java/com/zhao/myutils/utils/AppManagerUtil.java
@@ -51,4 +51,29 @@ public class AppManagerUtil {
         }
         Process.killProcess(Process.myPid());
     }
+
+    /**
+     * 用来判断服务是否运行.
+     *
+     * @param className 判断的服务名字
+     * @return true 在运行 false 不在运行
+     */
+    public static boolean isServiceRunning(Context context, String className) {
+        boolean isRunning = false;
+        ActivityManager activityManager = (ActivityManager)
+                context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> serviceList
+                = activityManager.getRunningServices(30);
+        if (!(serviceList.size() > 0)) {
+            return false;
+        }
+        for (int i = 0; i < serviceList.size(); i++) {
+            if (serviceList.get(i).service.getClassName().equals(className)) {
+                isRunning = true;
+                break;
+            }
+        }
+        return isRunning;
+    }
+
 }
