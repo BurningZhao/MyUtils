d6b250c6b0ba905e4db1169e17f1ff269d2f6f5b
qingbo <bluesky2008bo@126.com>
2016-07-08 11:07:43 +0800

    refactor

--- MyUtils/src/main/java/com/zhao/myutils/base/BaseApplication.java
+++ MyUtils/src/main/java/com/zhao/myutils/base/BaseApplication.java
@@ -10,15 +10,15 @@ import com.zhao.myutils.utils.CrashHandler;
  * @author zhaoqingbo
  * @since 2016/4/27
  */
-public class BaseApplication extends Application{
+public class BaseApplication extends Application {
 
     @Override
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
         initErrorHandler();
     }
 
-    private void initErrorHandler(){
+    private void initErrorHandler() {
         CrashHandler handler = CrashHandler.getInstance();
         handler.init(this);
     }
